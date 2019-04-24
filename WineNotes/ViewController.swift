c14e745d0db6f176bbedf7f9fce3a06113845dcc
Josh Bernitt <josh@zeroedin.com>
2019-04-23 20:22:56 -0400

    Initial Commit

---
+++ WineNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WineNotes
+//
+//  Created by Joshua Bernitt on 4/23/19.
+//  Copyright © 2019 Joshua Bernitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
