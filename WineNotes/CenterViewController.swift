b1d9384e5db3ac2a44c131d78761656f29299b4a
Josh Bernitt <josh@zeroedin.com>
2019-04-25 21:42:20 -0400

    slowly working on navigation views

---
+++ WineNotes/CenterViewController.swift
@@ -0,0 +1,11 @@
+//
+//  CenterViewController.swift
+//  WineNotes
+//
+//  Created by Joshua Bernitt on 4/25/19.
+//  Copyright © 2019 Joshua Bernitt. All rights reserved.
+//
+
+import UIKit;
+
+class CenterViewController: UIViewController {}
---
+++ WineNotes/SideViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SideView.swift
+//  WineNotes
+//
+//  Created by Joshua Bernitt on 4/25/19.
+//  Copyright © 2019 Joshua Bernitt. All rights reserved.
+//
+
+import UIKit
+
+class SideViewController: UIViewController {}
