65f94041adac4e6d9f43dc4a7a859da08dc02125
bj7 <jjbernitt@gmail.com>
2019-11-25 17:49:20 -0500

    Enables keyboard inset to prevent hiding of text Scroll View now shifts up when the keyboard becomes active. On tap event added to main view to hide keyboard when focus on current text field is lost.

--- WineNotes/NoteEntryViewController.swift
+++ WineNotes/NoteEntryViewController.swift
@@ -12,13 +12,20 @@ class NoteEntryViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     var years:[Int] = [];
     let date = Date();
     let calendar = Calendar.current;
+    var activeTextField: UITextField!;
     
+    // called by on tap event
+    @IBAction func hideKeyboard(_ sender: AnyObject) {
+        self.activeTextField.endEditing(true);
+        self.activeTextField = nil;
+    }
     @IBOutlet weak var yearPicker: UIPickerView!
     @IBOutlet weak var wineTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var regionTextField: UITextField!
     @IBOutlet weak var varietyTextField: UITextField!
     @IBOutlet weak var recommendedTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         let year = calendar.component(.year, from: date);
@@ -26,29 +33,42 @@ class NoteEntryViewController: UIViewController, UIPickerViewDelegate, UIPickerV
             years.append(i)
         }
         
-        
         wineTextField.delegate = self;
-        
         priceTextField.delegate = self;
         self.priceTextField.keyboardType = UIKeyboardType.decimalPad;
-        
         yearPicker.dataSource = self;
         yearPicker.delegate = self;
         yearPicker.selectRow(years.count - 3, inComponent: 0, animated: true);
-        
         regionTextField.delegate = self;
-        
         varietyTextField.delegate = self;
-        
         recommendedTextField.delegate = self;
+        
+        // observers to handle when the keyboard comes into and
+        // out of focus
+        NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(keyboardWillShow(_:)),
+          name: UIResponder.keyboardWillShowNotification,
+          object: nil)
+
+        NotificationCenter.default.addObserver(
+          self,
+          selector: #selector(keyboardWillHide(_:)),
+          name: UIResponder.keyboardWillHideNotification,
+          object: nil)
     }
     
+    /**
+     Text Field Handling
+     */
+    // enables keyboard hiding upon return button click
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true);
-        wineTextField.resignFirstResponder();
+        self.activeTextField.resignFirstResponder();
         return true;
     }
     
+    // basic validation
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         //For numers
         if textField == priceTextField {
@@ -59,6 +79,36 @@ class NoteEntryViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         return true
     }
     
+    // Assign the newly active text field to activeTextField variable
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.activeTextField = textField
+    }
+    
+    // adjust keyboard inest height and correct for overlapping input elements
+    func adjustInsetForKeyboard(_ show: Bool, notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey]
+                as? NSValue else {
+            return;
+        }
+        
+        let adjustmentHeight = (keyboardFrame.cgRectValue.height + 20) * (show ? 1 : -1);
+        scrollView.contentInset.bottom += adjustmentHeight;
+        scrollView.verticalScrollIndicatorInsets.bottom += adjustmentHeight;
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        self.adjustInsetForKeyboard(true, notification: notification);
+    }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        self.adjustInsetForKeyboard(false, notification: notification);
+    }
+    
+    
+    /**
+     Picker handling
+     */
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1;
     }
